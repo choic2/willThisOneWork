5d64c0cde13e2cbfaea9bb8f27930de7b635dfea
Choi <choic2@seattleu.edu>
2018-05-15 22:51:40 -0700

    Add project files.

---
+++ 5051/Models/AvatarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5051.Models
+{
+    /// <summary>
+    /// View Model for the Avatar Views to have the list of Avatars
+    /// </summary>
+    public class AvatarViewModel
+    {
+        /// <summary>
+        /// The List of Avatars
+        /// </summary>
+        public List<AvatarModel> AvatarList = new List<AvatarModel>();
+    }
+}
